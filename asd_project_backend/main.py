d45bc76701ecba1d4b28cdf200785daf525088c8
hemalatha <hemalathabora9@gmail.com>
2026-02-22 13:18:01 +0530

    connect frontend to live backend

--- asd_project_backend/main.py
+++ asd_project_backend/main.py
@@ -18,8 +18,7 @@ app = FastAPI(title="NeuroMate – ASD Adaptive Screening API (Final Logic)")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
+        "*",
     ],
     allow_credentials=True,
     allow_methods=["*"],
